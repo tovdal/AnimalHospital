a8737881ebe5b04810c4bf46356bc7dace15d3a8
Tova Dalin <tova.dalin@hotmail.com>
2024-12-04 09:34:14 +0100

    Add project files.

---
+++ AnimalHospital/Animal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalHospital
+{
+    public class Animal
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public int Legs { get; set; }
+        public string Age { get; set; }
+        public bool IsVaccinated { get; set; }
+
+    }
+}
---
+++ AnimalHospital/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace AnimalHospital
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            var animals = new List<Animal>
+            {
+                
+            };
+            
+        }
+    }
+}
